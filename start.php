a4cf89733634ea8967acfbae81b2288056e4c1bd
Diyalaxmi <diyalaxmijoshi@gmail.com>
2016-02-23 10:11:11 +0545

    test

---
+++ start.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first php page </h1>
+<?php
+// this is a single line comment
+
+#this is also a single line comment
+
+/*this is a multiple line comment*/
+
+
+echo "hello world!! <br>";
+
+// Okay let's do some simple addition and substraction hahah <3
+$a = 1;
+$b = 2;
+
+$x = $a + $b;
+echo $x;
+// php is not case sensitive 
+
+ECHO "<br>Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+
+// this for the variable test 
+
+$color = 'red';
+echo "My car is " . $color. "<br>";
+echo "My house is " .$color. "<br>";
+echo "My book is " .$color. "<br>";
+
+
+?>
+
+</body>
+</html>
